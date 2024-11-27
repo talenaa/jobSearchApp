2bed4be39b704db4eb2b19fe40120d47a0ba0d4b
talenaa <talenabarbosa7@gmail.com>
2024-11-27 15:36:15 +0100

    feat: added thead and its contents

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,1 +1,15 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="journals">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Company</th>
+                    <th scope="col">Offer</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Applied</th>
+                </tr>
+            </thead>
+@endsection
