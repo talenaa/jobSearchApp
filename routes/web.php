e33126b95224da88dceafdf6b05d82f95f85fa06
talenaa <talenabarbosa7@gmail.com>
2024-11-29 08:33:10 +0100

    feat: web.php route is done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OfferController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [OfferController::class, 'index'])->name('home');
