dfe59a67b2750313adff441dacf7d81b9049f52d
talenaa <talenabarbosa7@gmail.com>
2024-11-29 10:00:02 +0100

    feat: added update route

--- routes/api/api.php
+++ routes/api/api.php
@@ -4,5 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\OfferController;
 
-Route::get('/offers',[OfferController::class,'index'])->name('apihome');
-Route::delete('/offers/{id}',[OfferController::class,'destroy'])->name('apidestroy');
+Route::get('/offers',[OfferController::class, 'index'])->name('apihome');
+Route::delete('/offers/{id}',[OfferController::class, 'destroy'])->name('apidestroy');
+Route::put('/offers/{id}',[OffersController::class, 'update'])->name('apiupdate');
