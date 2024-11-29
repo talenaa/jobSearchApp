094c43d8703cdd239e89e51d97629f54ea4c6edb
talenaa <talenabarbosa7@gmail.com>
2024-11-29 10:08:19 +0100

    feat: added store route

--- routes/api/api.php
+++ routes/api/api.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\Api\OfferController;
 Route::get('/offers',[OfferController::class, 'index'])->name('apihome');
 Route::delete('/offers/{id}',[OfferController::class, 'destroy'])->name('apidestroy');
 Route::put('/offers/{id}',[OffersController::class, 'update'])->name('apiupdate');
+Route::post('/offers',[OffersController::class, 'store'])->name('apistore');
