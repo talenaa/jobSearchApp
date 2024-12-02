a93a66f2da61474330302160d1d79cf4e9b602b3
talenaa <talenabarbosa7@gmail.com>
2024-12-02 13:08:08 +0100

    refactor: changed line order

--- app/Http/Controllers/api/OfferController.php
+++ app/Http/Controllers/api/OfferController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Offer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class OfferController extends Controller
 {
