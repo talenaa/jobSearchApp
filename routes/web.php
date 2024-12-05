7cae93620aca8d787aaa119580eff131c494d4ea
talenaa <talenabarbosa7@gmail.com>
2024-12-05 14:53:50 +0100

    feat: added show route in web

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OfferController;
 
 Route::get('/', [OfferController::class, 'index'])->name('home');
+Route::get('/offers/{id}', [OfferController::class, 'show'])->name('show');
