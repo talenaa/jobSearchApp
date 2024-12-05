0f5e20da8f04e305ddcc1dd124f446889b94483c
talenaa <talenabarbosa7@gmail.com>
2024-12-05 07:53:17 +0100

    docs: made model file

--- app/Http/Controllers/api/OfferController.php
+++ app/Http/Controllers/api/OfferController.php
@@ -21,10 +21,10 @@ class OfferController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    /* public function create()
     {
         //
-    }
+    } */
 
     /**
      * Store a newly created resource in storage.
