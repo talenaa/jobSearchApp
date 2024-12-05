b5dbe9d3647aa03f5951905b0e3e505fd6a56052
talenaa <talenabarbosa7@gmail.com>
2024-12-05 15:15:24 +0100

    feat: added index and show function

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class OfferController extends Controller
 {
-    //
+     /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $offers = Offer::all();
+
+        return view('home', compact('offers'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $offer = Offer::findOrFail($id);
+        return view('show', compact('offer'));
+    }
 }
