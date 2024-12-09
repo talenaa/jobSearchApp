cd9837dd4805299dd649fc4e1e107f42a825c960
talenaa <talenabarbosa7@gmail.com>
2024-12-10 00:04:00 +0100

    refactor: commented lines at user.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,37 +12,37 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
+  /*  *
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+    /* protected $fillable = [
         'name',
         'email',
         'password',
-    ];
+    ]; */
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
+    /* protected $hidden = [
         'password',
         'remember_token',
-    ];
+    ]; */
 
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
-    protected function casts(): array
+   /*  protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
+    } */
 }
