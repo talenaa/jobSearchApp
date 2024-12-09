3ab7f5c60c2cdb9b99a071af96851c0d9d2d2317
talenaa <talenabarbosa7@gmail.com>
2024-12-09 14:33:02 +0100

    feat: adding tests

---
+++ tests/Feature/Api/OfferTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use App\Models\Offer;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class OfferTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_CheckIfCanReceiveAllOffersInJsonFile() {
+        $offer = Offer::factory(6)->create();
+        $response = $this->get(route('apihome'));
+
+        $response->assertStatus(200)
+            ->assertJsonCount(6);
+    }
+}
---
+++ tests/Feature/OfferTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Offer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class OfferTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_IndexIsWorking()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->get(route('index'));
+        $response->assertStatus(200)
+                ->assertViewIs('index');
+    }
+}
