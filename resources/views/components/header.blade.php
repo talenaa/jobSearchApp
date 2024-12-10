e5012b2249ab56a03428a7c5d255970605690e63
talenaa <talenabarbosa7@gmail.com>
2024-12-10 12:27:33 +0100

    style: home css done

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,6 @@
 <header>
     <img src="{{ asset('css/img/jobSearchLogo.png') }}" alt="logo">
-    <h1>Job Search</h1>
+        <div class="title">
+           <h1>Job Search</h1>
+        </div>
 </header>
