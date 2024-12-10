768b5c73813937351046c4df7f2461b3ff909e1b
talenaa <talenabarbosa7@gmail.com>
2024-12-10 14:59:49 +0100

    style: flex done

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,26 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="offer">
-        <h2 class="offer->feedback">Entry</h2>
-        <div class="offerTime">
-            <table>
-                <th scope="col">Comments</th>
-                <th scope="col">Created</th>
-                <th scope="col">Updated</th>
-                
-                @foreach ($offer->feedback as $news)
+        <div class="offer">
+            <h2 class="titleShow">Entries</h2>
+            <div class="offerTime">
+                <table class="table table-bordered">
+                    <tr>
+                        <th scope="col">Comments</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Updated</th>
+                    </tr>
+                    
+                    @foreach ($offer->feedback as $news)
 
-                <tr>
-                    <td>{{$news->news}}</td>
-                    <td>{{$news->updated_at}}</td>
-                    <td>{{$news->created_at}}</td>
-                </tr>
-                @endforeach
-            </table>
+                    <tr>
+                        <td>{{$news->news}}</td>
+                        <td>{{$news->updated_at}}</td>
+                        <td>{{$news->created_at}}</td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+
+            <div class="offerContent">
+                <p class="offerEntry">{{$offer['entry']}}</p>
+            </div>
         </div>
-        <div class="offerContent">
-            <p class="offerEntry">{{$offer['entry']}}</p>
-        </div>
-    </div>
 @endsection
