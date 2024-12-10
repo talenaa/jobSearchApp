380d5aa6b01a272fb8c4733f2c0d2fc0a9338cf3
talenaa <talenabarbosa7@gmail.com>
2024-12-10 11:01:08 +0100

    fix: route img

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class ="personalInfo">
         <a href="mailto:talenabarbosa7@gmail.com" target="_blank">
-            <img src="{{ asset('img/icons/gmailIcon.webp') }}" alt="gmail icon">
+            <img src="{{ asset('css/img/icons/gmailIcon.webp') }}" alt="gmail icon">
         </a>
 
         <a href="https://github.com/talenaa">
-            <img src="{{ asset('img/icons/gitHubIcon.png') }}" alt="git icon">
+            <img src="{{ asset('css/img/icons/gitHubIcon.png') }}" alt="git icon">
         </a>
         
         <p>© Talena Barbosa.2024</p>
