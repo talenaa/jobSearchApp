c924d149bd144bc56aebafaf5bf56371b36b1782
talenaa <talenabarbosa7@gmail.com>
2024-12-10 21:23:32 +0100

    feat: imported css url

--- tests/Feature/OfferTest.php
+++ tests/Feature/OfferTest.php
@@ -28,4 +28,100 @@ class OfferTest extends TestCase
         $response->assertStatus(200);
         
     }
+
+    public function test_storeMethodSavesObjectCorrectly()
+    {
+        $response = $this->post(route('store'), [
+            'title' => 'New Offer',
+            'enterprise' => 'Hello enterprise',
+            'description' => 'Hello description',
+            'workspace' => 'Hello workspace',
+            'status' => true
+        ]);
+
+        $response = $this->get(route('home'));
+        $response->assertStatus(200);
+        $this->assertDatabaseCount('offers', 1);
+    }
+
+    public function test_checkIfShowViewWorksCorrectly()
+    {
+        $response = $this->post(route('store'), [
+            'title' => 'New Offer',
+            'enterprise' => 'Hello enterprise',
+            'description' => 'Hello description',
+            'workspace' => 'Hello workspace',
+            'status' => true
+        ]);
+
+        $response = $this->get('/show/2');
+
+        $response->assertStatus(200)
+            ->assertViewIs('show');
+    }
+
+    public function test_checkIfEditFormViewWorksCorrectly()
+    {
+        $response = $this->post(route('store'), [
+            'title' => 'New Offer',
+            'enterprise' => 'Hello enterprise',
+            'description' => 'Hello description',
+            'workspace' => 'Hello workspace',
+            'status' => true
+        ]);
+
+        $response = $this->get('/edit/1');
+
+        $response->assertStatus(200)
+            ->assertViewIs('edit');
+    }
+
+    public function test_checkIfUpdateMethodWorksCorrectly()
+    {
+        $response = $this->post(route('store'), [
+            'title' => 'New Offer',
+            'enterprise' => 'Hello enterprise',
+            'description' => 'Hello description',
+            'workspace' => 'Hello workspace',
+            'status' => true
+        ]);
+
+        $response = $this->post(route('update', 1), [
+            'title' => 'New Offer',
+            'enterprise' => 'Hello',
+            'description' => 'Hello',
+            'workspace' => 'Hello',
+            'status' => true
+        ], );
+
+        $offer = Offer::find(1);
+        $this->assertEquals('Hello', $offer->enterprise);
+
+    }
+
+    public function test_checkIfDeleteMethodDestroyElement()
+    {
+        $response = $this->post(route('store'), [
+            'title' => 'New Offer',
+            'enterprise' => 'Hello enterprise',
+            'description' => 'Hello description',
+            'workspace' => 'Hello workspace',
+            'status' => true
+        ]);
+
+        $response = $this->delete(route('destroy',1));
+        $this->assertDatabaseCount('offers', 0);
+    }
+
+    public function test_checkIfRedirectViaActionToDeleteWorks(){
+        $response = $this->post(route('store'), [
+            'title' => 'New Offer',
+            'enterprise' => 'Hello enterprise',
+            'description' => 'Hello description',
+            'workspace' => 'Hello workspace',
+            'status' => true
+        ]);
+        $this->get('/?action=delete&id=1');
+        $this->assertDatabaseCount('offers', 0);
+    }
 }
